0cceb97089689badf5afec53e88736d29aa863bc
Thomas Massey <thomassey476@outlook.com>
2022-11-10 22:44:05 +0000

    Added is empty or full

--- main.py
+++ main.py
@@ -14,17 +14,28 @@ class Stack:
         self.head = None
         self.tail = None
         self.size = 0
+        self.max_size = 10
     
     def push(self, data):
-        node = Node(data)
-        if self.head is None:
-            self.head = node
-            self.tail = node
+        if self.size == self.max_size:
+            raise Exception("Stack is full")
+            return None
         else:
-            node.prev = self.tail
-            self.tail.next = node
-            self.tail = node
-        self.size += 1
+            node = Node(data)
+            if self.head is None:
+                self.head = node
+                self.tail = node
+            else:
+                node.prev = self.tail
+                self.tail.next = node
+                self.tail = node
+            self.size += 1
+        
+    def is_empty(self):
+        return self.head is None
+
+    def is_full(self):
+        return self.size == self.max_size
     
     def pop(self):
         if self.head is None:
